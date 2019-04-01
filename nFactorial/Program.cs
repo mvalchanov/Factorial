7fad45f3539459dafaf9622d111f7dc295f9e03a
Martin V <m.valchanov@abv.bg>
2019-04-01 15:16:35 +0300

    Simple Factorial func

---
+++ nFactorial/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+
+namespace nFactorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            Console.WriteLine("Press <Enter> or write \"end\" to exit \n");
+            do
+            {
+                Console.Write("Insert number: ");
+
+                inputString = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(inputString) || inputString == "end")
+                {
+                    break;
+                }
+
+                bool isNumber = int.TryParse(inputString, out int parsedNumber);
+
+
+                if (isNumber)
+                {
+                    BigInteger result = CalculateFactorial(parsedNumber);
+
+                    Console.WriteLine($"\t {parsedNumber}! = {result} \n");
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number. \n");
+
+                }
+
+            } while (inputString != "end");
+
+        }
+
+        private static BigInteger CalculateFactorial(int number)
+        {
+            BigInteger result = 1;
+
+            if (number == 0)
+            {
+                return result;
+            }
+
+            for (int i = number; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+
+        }
+    }
+}
